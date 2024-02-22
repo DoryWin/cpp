a11cf66a3ddc3e51a02d692b7e8aa0bbfbfe48d7
DoryWin <159138874+DoryWin@users.noreply.github.com>
2024-02-22 20:22:34 +0700

    Add files via upload

---
+++ ex1_int.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int no1;
+    int no2;
+    std::cout << "Enter number 1: " << std::endl;
+    std::cin >> no1;
+    std::cout << "Enter number 2: " << std::endl;
+    std::cin >> no2;
+    std::cout << no1 + no2 << std::endl;
+    return 0;
+}
---
+++ ex1_nameage.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::string user_name;
+    int user_age;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> user_name;
+    std::cout << "How old are you?" << std::endl;
+    std::cin >> user_age;
+    std::cout << "Hello" << user_name << ", you are " << user_age << "." << std::endl; 
+    return 0;
+ }
